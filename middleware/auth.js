be44e58a8f5a60a1a3c56426032057ce9a3a0dd0
Eliot Raven <eliotraven@gmail.com>
2018-12-12 08:59:38 +0200

    rm console.logs

--- middleware/auth.js
+++ middleware/auth.js
@@ -4,7 +4,6 @@ export default function (context) {
     let authUser = (process.server) ? auth.getUserFromCookie(context.req) :auth.getToken()
 
     if (!authUser) {
-        console.log('auth ', authUser)
         return context.redirect(200, '/login')
     }
 
--- middleware/check-auth.js
+++ middleware/check-auth.js
@@ -3,22 +3,6 @@ import {auth} from "../assets/controllers/auth";
 export default function (context) {
 
     const authUser = (process.server) ? auth.getUserFromCookie(context.req) : auth.getToken()
-
-    console.log('chek-auth ', authUser)
-
     return
 
-    // if (process.server) {
-    //     if (!authUser) {
-    //         // context.redirect(200, '/login')
-    //     } else {
-    //         // context.redirect(200, '/home')
-    //     }
-    // }
-    //
-    // if (context.isServer && !context.req) {
-    //     // context.redirect(200, '/home')
-    // }
-    //
-    // return
 }
